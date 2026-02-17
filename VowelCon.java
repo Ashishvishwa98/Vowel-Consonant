f9f8b7f3a361405ae45742cc6ae40d2ebb979159
Ashish Vishwakarma <ashishvishwa9898@gmail.com>
2026-02-17 23:26:43 +0530

    Add files via upload

---
+++ VowelCon.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+public class VowelCon {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Alphabet:");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e'
+                || ch == 'i'
+                || ch == 'o'
+                || ch == 'u'
+                || ch == 'A'
+                || ch == 'E'
+                || ch == 'I'
+                || ch == 'O'
+                || ch == 'U') {
+            System.out.println("Given character is Vowel");
+        } else {
+            System.out.println("Given Character Consonant");
+        }
+
+    }
+}
